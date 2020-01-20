6d8eabf3a2cf6a945e9a25eee21ea8422bcadd3f
Adrián Gallardo <adrian.gallardo@wizeline.com>
2020-01-20 14:16:37 -0600

    chore: removed unused files

--- example/src/counter/provider/index.ts
+++
@@ -1,1 +0,0 @@
-export { default, withCounter, useCounter } from './CounterProvider';
--- example/src/counter/useCountLogger.ts
+++
@@ -1,9 +0,0 @@
-import { useCounter } from './provider';
-
-export default () => {
-    const { count } = useCounter({
-      stateMappers: ['count'],
-    });
-  
-    console.warn(`Count is: ${count}`);
-}
