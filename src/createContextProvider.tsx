5dd4d99d0262adf1ccb9d31f94daaf58111b24b4
Adrián Gallardo <adrian.gallardo@wizeline.com>
2020-02-10 19:22:32 -0600

    feat: adds providerContextMap

--- src/createContextProvider.tsx
+++ src/createContextProvider.tsx
@@ -4,34 +4,35 @@ import {
     KeyValue,
     ActionCreators,
     CreateContextProviderReturn,
+    CustomProvider,
 } from './typings';
 
-  
+const providerContextMap = new Map<CustomProvider, React.Context<any>>();
+
 function createContextProvider(reducer: React.Reducer<any, any>, initialState: KeyValue, actionCreators: ActionCreators): CreateContextProviderReturn {
     const Context = React.createContext({});
+    const Provider = ({ onInit, ...props }: KeyValue) => {
+        const [state, dispatch] = React.useReducer(reducer, initialState);
+        const actions = getBindedActions(actionCreators, dispatch, state);
 
-    return [
-        ({ onInit, ...props }: KeyValue) => {
-            const [state, dispatch] = React.useReducer(reducer, initialState);
-            const actions = getBindedActions(actionCreators, dispatch, state);
+        const contextValue = {
+            state: Object.freeze(state),
+            actions: Object.freeze(actions),
+        };
 
-            const contextValue = {
-                state: Object.freeze(state),
-                actions: Object.freeze(actions),
-            };
+        if (onInit) {
+            React.useMemo(() => {
+                onInit(contextValue);
+            }, [onInit]);
+        }
 
-            if (onInit) {
-                React.useMemo(() => {
-                    onInit(contextValue);
-                }, [onInit]);
-            }
+        return (
+            <Context.Provider value={contextValue} {...props}></Context.Provider>
+        );
+    };
+    providerContextMap.set(Provider, Context);
 
-            return (
-                <Context.Provider value={contextValue} {...props}></Context.Provider>
-            );
-        },
-        Context,
-    ]
+    return [Provider, Context];
     
 }
 
@@ -45,4 +46,16 @@ function getBindedActions(actions: ActionCreators, dispatch: React.Dispatch<any>
     return bindedActions;
 }
 
+function getProviderContext(Provider: CustomProvider): React.Context<any> {
+    const Context = providerContextMap.get(Provider);
+
+    if (!Context) {
+        throw `Context not found for Provider ${Provider}`;
+    }
+    return Context;
+}
+
 export default createContextProvider;
+export {
+    getProviderContext,
+};
