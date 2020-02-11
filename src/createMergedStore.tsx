aaa8c546d3d3d4da8b547435a7b5b0448597d7d0
Adrián Gallardo <adrian.gallardo@wizeline.com>
2020-02-10 23:37:56 -0600

    feat: createMergedStore

---
+++ src/createMergedStore.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { mergedConnectContextFactory, useMergedConnectedContextFactory } from './connectContext';
+import { getProviderContext } from './createContextProvider';
+
+import { CustomProvider, ConnectContextFactory, KeyValue, ConnectContextOptions } from "./typings";
+
+const DefaultChild = ({ children }: any) => children;
+
+function createMergedStore(providers: CustomProvider[]): [CustomProvider, ConnectContextFactory, Function] {
+    const contexts = providers.map(Provider => getProviderContext(Provider));
+    const withStore = mergedConnectContextFactory(contexts);
+    const useStore = useMergedConnectedContextFactory(contexts);
+
+    const MergedStore = (props: KeyValue) => {
+        const Child = props.onInit ?  getConnectedChild(withStore, props.onInit) : DefaultChild;
+
+        let wrapper: any = <Child {...props} />;
+
+        providers.forEach((Provider) => {
+            const previousWrapper = wrapper;
+
+            wrapper = <Provider>{ previousWrapper }</Provider>
+        });
+
+        return React.useMemo(() => wrapper, providers);
+    }
+
+    return [
+        MergedStore,
+        withStore,
+        useStore,
+    ];
+}
+
+function getConnectedChild(withStore: ConnectContextFactory, onInit: [ConnectContextOptions, Function]) {
+    const [options, onInitFn] = onInit;
+
+    return withStore(({ children, ...selectionResult }: KeyValue) => {
+        React.useMemo(() => {
+            onInitFn(selectionResult);
+        }, []);
+
+        return children
+    }, options);
+}
+
+export default createMergedStore;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { ContextType } from 'react';
 
-import connectContext, { connectContextFactory, useConnectedContextFactory, mergedConnectContextFactory } from './connectContext';
+import connectContext, { connectContextFactory, useConnectedContextFactory, mergedConnectContextFactory, useMergedConnectedContextFactory } from './connectContext';
 import createContextProvider from './createContextProvider';
+import createMergedStore from './createMergedStore';
 
 export default createContextProvider;
 export {
@@ -9,5 +10,7 @@ export {
   connectContextFactory,
   useConnectedContextFactory,
   mergedConnectContextFactory,
+  useMergedConnectedContextFactory,
+  createMergedStore,
   ContextType,
 };
